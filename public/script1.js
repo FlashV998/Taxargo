4b23491b1e685ab3e4b6628f75236442727b0249
FLASH <chetanya002@gmail.com>
2020-08-14 17:11:14 +0530

    added login and registration page

--- public/script1.js
+++ public/script1.js
@@ -2,27 +2,27 @@ document.addEventListener("DOMContentLoaded", () => {
     class MultiStep {
      constructor(formId) {
       let myForm = document.querySelector(formId),
-       steps = myForm.querySelectorAll(".steps"),
+       steps = myForm.querySelectorAll(".inner-section-step"),
        btnPrev = myForm.querySelector(".btnPrev"),
        btnNext = myForm.querySelector(".btnNext"),
-       indicators = myForm.querySelectorAll(".rounded-circle"),
+      //  indicators = myForm.querySelectorAll(".rounded-circle"),
        inputClasses = ".form-control",
        currentTab = 0,
        counter=0,
-       button_1= myForm.querySelector("#clickMe-1"),
-       button_2= myForm.querySelector("#clickMe-2"),
-       button_3= myForm.querySelector("#clickMe-3"),
-       button_b_1= myForm.querySelector("#clickMe_b-1"),
-       removeb=myForm.querySelector("#remove"),
-       option1=myForm.querySelector(".option1"),
-       option2=myForm.querySelector(".option2"),
-       option3=myForm.querySelector(".option3"),
-       option4=myForm.querySelector(".option4"),
-       option5=myForm.querySelector(".option5");
+      //  button_1= myForm.querySelector("#clickMe-1"),
+      //  button_2= myForm.querySelector("#clickMe-2"),
+      //  button_3= myForm.querySelector("#clickMe-3"),
+      //  button_b_1= myForm.querySelector("#clickMe_b-1"),
+       removeb=myForm.querySelector("#remove");
+      //  option1=myForm.querySelector(".option1"),
+      //  option2=myForm.querySelector(".option2"),
+      //  option3=myForm.querySelector(".option3"),
+      //  option4=myForm.querySelector(".option4"),
+      //  option5=myForm.querySelector(".option5");
        
        
     //hidding functions step:4
-   
+      
     $("#show-button-4-1").click(function(){
         $("#show-4-1").toggle(200);
          $("#show-button-4-1").hide();
@@ -85,24 +85,14 @@ document.addEventListener("DOMContentLoaded", () => {
        
       }
    
-      function showIndicators(n) {
-       for (let i = 0; i < indicators.length; i++) {
-        indicators[i].classList.replace("bg-warning", "bg-success");
-       }
-       indicators[n].className += " bg-warning";
-      }
+      // function showIndicators(n) {
+      //  for (let i = 0; i < indicators.length; i++) {
+      //   indicators[i].classList.replace("bg-warning", "bg-success");
+      //  }
+      //  indicators[n].className += " bg-warning";
+      // }
    
-      function clickerBtn(n) {
-       if (n == 1 && !validateForm()) return false;
-       steps[currentTab].classList.remove("active");
-       currentTab = currentTab + n;
-       if (currentTab >= steps.length) {
-        myForm.submit();
-        return false;
-       }
-       showTab(currentTab);
-       showIndicators(currentTab);
-      }
+      
    // Do whatever validation you want
       function validateForm() {
        let input = steps[currentTab].querySelectorAll(inputClasses),
@@ -123,6 +113,18 @@ document.addEventListener("DOMContentLoaded", () => {
        return valid;
       }
       
+      function clickerBtn(n) {
+         if (n == 1 && !validateForm() ) return false;
+         steps[currentTab].classList.remove("active");
+         currentTab = currentTab + n;
+         if (currentTab >= steps.length) {
+          myForm.submit();
+          return false;
+         }
+         showTab(currentTab);
+        //  showIndicators(currentTab);
+        }
+
       btnPrev.addEventListener("click", () => {
        clickerBtn(-1);
       });
@@ -130,56 +132,56 @@ document.addEventListener("DOMContentLoaded", () => {
        clickerBtn(1);
        });
       
-      option1.addEventListener("click",()=>{
-         currentTab=0;
-         for(let j=0;j< steps.length;j++){
-            steps[j].classList.remove("active");
-            }
-         showTab(currentTab);
-      });
-      option2.addEventListener("click",()=>{
-         currentTab=1;
-         for(let j=0;j< steps.length;j++){
-            steps[j].classList.remove("active");
-            }
-         showTab(currentTab);
-      });
-      option3.addEventListener("click",()=>{
-         currentTab=2;
-         for(let j=0;j< steps.length;j++){
-            steps[j].classList.remove("active");
-            }
-         showTab(currentTab);
-      });
-      option4.addEventListener("click",()=>{
-         currentTab=3;
-         for(let j=0;j< steps.length;j++){
-            steps[j].classList.remove("active");
-            }
-         showTab(currentTab);
-      }); 
-      option5.addEventListener("click",()=>{
-         currentTab=4;
-         for(let j=0;j< steps.length;j++){
-            steps[j].classList.remove("active");
-            }
-         showTab(currentTab);
-      });
-      button_1.addEventListener("click",()=>{
-         $(".button-container-1").append('<div class="form-row"> <div class="form-group col-md-12"><div class="row"> <div class="col-md-4"><p>Basic Salary</p></div><div class="col-md-4"><input type="text" class="form-control" id="div_sal-1"  required></div><div class="col-md-4"><button id="remove" type="button">X</button></div></div></div></div>')
-      });
-      button_2.addEventListener("click",()=>{
-         $(".button-container-2").append('<div class="form-row"> <div class="form-group col-md-12"><div class="row"> <div class="col-md-4"><p>Basic Salary</p></div><div class="col-md-4"><input type="text" class="form-control" id="div_sal-1"  required></div><div class="col-md-4"><button id="remove" type="button">X</button></div></div></div></div>')
-      });
-      button_3.addEventListener("click",()=>{
-         $(".button-container-3").append('<div class="form-row"> <div class="form-group col-md-12"><div class="row"> <div class="col-md-4"><p>Basic Salary</p></div><div class="col-md-4"><input type="text" class="form-control" id="div_sal-1"  required></div><div class="col-md-4"><button id="remove" type="button">X</button></div></div></div></div>')
-      });
-      button_b_1.addEventListener("click",()=>{
-         $(".button-container_b-1").append('<div class="form-row"> <div class="form-group col-md-12"><div class="row"> <div class="col-md-4"><p>Basic Salary</p></div><div class="col-md-4"><input type="text" class="form-control" id="div_sal-1"  required></div><div class="col-md-4"><button id="remove" type="button">X</button></div></div></div></div>')
-      });
-      removeb.addEventListener("click",()=>{
-         $("#remove").parent("div").remove();
-      });
+      // option1.addEventListener("click",()=>{
+      //    currentTab=0;
+      //    for(let j=0;j< steps.length;j++){
+      //       steps[j].classList.remove("active");
+      //       }
+      //    showTab(currentTab);
+      // });
+      // option2.addEventListener("click",()=>{
+      //    currentTab=1;
+      //    for(let j=0;j< steps.length;j++){
+      //       steps[j].classList.remove("active");
+      //       }
+      //    showTab(currentTab);
+      // });
+      // option3.addEventListener("click",()=>{
+      //    currentTab=2;
+      //    for(let j=0;j< steps.length;j++){
+      //       steps[j].classList.remove("active");
+      //       }
+      //    showTab(currentTab);
+      // });
+      // option4.addEventListener("click",()=>{
+      //    currentTab=3;
+      //    for(let j=0;j< steps.length;j++){
+      //       steps[j].classList.remove("active");
+      //       }
+      //    showTab(currentTab);
+      // }); 
+      // option5.addEventListener("click",()=>{
+      //    currentTab=4;
+      //    for(let j=0;j< steps.length;j++){
+      //       steps[j].classList.remove("active");
+      //       }
+      //    showTab(currentTab);
+      // });
+      // button_1.addEventListener("click",()=>{
+      //    $(".button-container-1").append('<div class="form-row"> <div class="form-group col-md-12"><div class="row"> <div class="col-md-4"><p>Basic Salary</p></div><div class="col-md-4"><input type="text" class="form-control" id="div_sal-1"  required></div><div class="col-md-4"><button id="remove" type="button">X</button></div></div></div></div>')
+      // });
+      // button_2.addEventListener("click",()=>{
+      //    $(".button-container-2").append('<div class="form-row"> <div class="form-group col-md-12"><div class="row"> <div class="col-md-4"><p>Basic Salary</p></div><div class="col-md-4"><input type="text" class="form-control" id="div_sal-1"  required></div><div class="col-md-4"><button id="remove" type="button">X</button></div></div></div></div>')
+      // });
+      // button_3.addEventListener("click",()=>{
+      //    $(".button-container-3").append('<div class="form-row"> <div class="form-group col-md-12"><div class="row"> <div class="col-md-4"><p>Basic Salary</p></div><div class="col-md-4"><input type="text" class="form-control" id="div_sal-1"  required></div><div class="col-md-4"><button id="remove" type="button">X</button></div></div></div></div>')
+      // });
+      // button_b_1.addEventListener("click",()=>{
+      //    $(".button-container_b-1").append('<div class="form-row"> <div class="form-group col-md-12"><div class="row"> <div class="col-md-4"><p>Basic Salary</p></div><div class="col-md-4"><input type="text" class="form-control" id="div_sal-1"  required></div><div class="col-md-4"><button id="remove" type="button">X</button></div></div></div></div>')
+      // });
+      // removeb.addEventListener("click",()=>{
+      //    $("#remove").parent("div").remove();
+      // });
      }
     }
     let MS = new MultiStep("#stepped");
--- server.js
+++ server.js
@@ -1,22 +1,87 @@
 const express=require("express");
 const bodyParser=require('body-parser');
-
+const authRoute=require('./routes/auth-routes/authRoutes');
+const mongoose=require('mongoose');
+const encrypt=require('mongoose-encryption');
 const app=express();
 app.set("view engine",'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+// DB CONNECTION AND CREATION OF COLLECTION
+mongoose.connect("mongodb://localhost:27017/rtiUserdb",{useNewUrlParser:true,useUnifiedTopology: true });
+
+const userLoginSchema=new mongoose.Schema({
+  email:String,
+  password:String
+});
+
+const User= new mongoose.model("User", userLoginSchema); //here model is created or collection document is created namely User
+
+
+
+
+// handle auth routes
+app.use('/auth',authRoute);
 
 app.get("/",function(req,res){
 
 res.render("index");
 });
 
-app.get("/test",function(req,res){
+app.get("/Form-page",function(req,res){
 
-  res.render("test.ejs");
+  res.render("Form-page.ejs");
   });
+
+app.get("/register",function(req,res){
+  res.render("register");
+
+});
+
+app.get("/login",function(req,res){
+  res.render("login");
+});
+
+app.post("/register",function(req,res){
+const newUser=new User({
+  email:req.body.username,
+  password:req.body.password
+});
+
+newUser.save(function(err){
+  if(err){
+    console.log(err);
+  }
+  else{
+  res.render("Form-page");
+  }
+});
+
+});
+
+
+app.post("/login",function(req,res){
+const CuserName=req.body.username;
+const Cpassword=req.body.password;
+User.findOne({email:CuserName},(err,foundUser)=>{
+            if(err){
+              console.log(err);
+            }
+            else{
+               if(foundUser.password === Cpassword){
+                 res.render("Form-page");
+               } }
+});
+});
+
+app.post("/Form-page",function(req,res){
+    const firstName=req.body.firstName;
+    const lastName=req.body.lastName;
+    console.log(firstName);
+
+});  
 
   app.listen(3000, function() {
     console.log("Server started on port 3000");
